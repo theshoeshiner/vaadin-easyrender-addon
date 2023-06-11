452455a07932254ac65dd4b45f0ccec18398cd61
daniel.watson <daniel.watson@bioclinica.com>
2023-06-11 07:53:51 -0400

    use valueproviders

--- addon/src/main/java/org/vaadin/addons/thshsh/easyrender/ImageRenderer.java
+++ addon/src/main/java/org/vaadin/addons/thshsh/easyrender/ImageRenderer.java
@@ -9,28 +9,40 @@ import com.vaadin.flow.function.ValueProvider;
 
 /**
  * 
- * Renderer that returns an Image based on url sourced from a ValueProvider, with a static width and height.
- * Useful when entities contain a url to a source image.
+ * Renderer that returns an Image with url, alt text, width, and height, specified via {@link ValueProvider}s
  *
  * @param <Source>
  */
 public class ImageRenderer<Source> extends ComponentRenderer<Image,Source> {
 	
     private static final long serialVersionUID = 5161427541175341534L;
-    
-    protected String width;
-    protected String height;
+
+    protected ValueProvider<Source, String> widthProvider;
+    protected ValueProvider<Source, String> heightProvider;
+    protected ValueProvider<Source, String> urlProvider;
+    protected ValueProvider<Source,String> altProvider;
 	
 	public ImageRenderer(ValueProvider<Source,String> urlProvider,ValueProvider<Source,String> altProvider,String width,String height) {
-		super(source -> {
-			Image image = new Image(urlProvider.apply(source), altProvider!=null? altProvider.apply(source):StringUtils.EMPTY);
-			image.setWidth(width);
-			image.setHeight(height);
-			return image;
-		});
-		this.width = width;
-		this.height = height;
+		this(urlProvider,altProvider,s -> width, s-> height);
 	}
 	
+    public ImageRenderer(ValueProvider<Source, String> urlProvider,ValueProvider<Source,String> altProvider, ValueProvider<Source, String> widthProvider,ValueProvider<Source, String> heightProvider) {
+        super();
+        this.urlProvider = urlProvider;
+        this.altProvider = altProvider;
+        this.widthProvider = widthProvider;
+        this.heightProvider = heightProvider;
+        
+    }
+
+    @Override
+    public Image createComponent(Source source) {
+        Image image = new Image(urlProvider.apply(source), altProvider!=null? altProvider.apply(source):StringUtils.EMPTY);
+        image.setWidth(widthProvider.apply(source));
+        image.setHeight(heightProvider.apply(source));
+        return image;
+    }
+	
+   
 
 }
